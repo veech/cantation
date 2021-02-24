322fbbc0e54844b5ebf07a8be105770dcb6de287
Alessandro Vecchi <avecchi.53@gmail.com>
2021-02-24 12:08:39 -0700

    feat: basic walk animations for player movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,30 @@
 extends KinematicBody2D
 
-const SPEED = 300
+const SPEED = 200
 
 var motion: Vector2 = Vector2(0, 0)
 
 func _physics_process(delta):
+	_move()
+	_animate()
+	
+	move_and_slide(motion)
+	
+func _move():
 	var x_direction: int = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var y_direction: int = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 	motion.x = x_direction * SPEED
 	motion.y = y_direction * SPEED
 	
-	move_and_slide(motion)
+func _animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("up_walk")
+	elif motion.y > 0:
+		$AnimatedSprite.play("down_walk")
+	elif motion.x < 0:
+		$AnimatedSprite.play("left_walk")
+	elif motion.x > 0:
+		$AnimatedSprite.play("right_walk")
+	else:
+		$AnimatedSprite.play("down_idle")
