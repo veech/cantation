9a83b47125129fbe65def4e2737858aa0a9a2a73
Reed Tucker <reeedt@gmail.com>
2021-02-28 08:03:41 -0500

    fixed some minor aesthetics in UI

--- Scripts/ItemSlot.gd
+++ Scripts/ItemSlot.gd
@@ -6,7 +6,7 @@ var item = null
 var style
 
 func _init():
-	rect_min_size = Vector2(36, 36)
+	rect_min_size = Vector2(52, 52)
 	style = StyleBoxFlat.new()
 	refreshColors()
 	style.set_border_width_all(3)
@@ -18,6 +18,7 @@ func set_item(new_item):
 	add_child(new_item)
 	item = new_item
 	item.item_slot = self
+	item.set_size(Vector2(64, 64))
 	
 	
 
