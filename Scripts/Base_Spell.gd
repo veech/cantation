8a54ce8038d4b91e542a77624fbc2fb4c226a6f3
Reed Tucker <reeedt@gmail.com>
2021-03-07 12:27:55 -0500

    Added base classes for spells and projectiles

---
+++ Scripts/Base_Spell.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var attributes = {}
+#in child classes with projectiles, one of the attributes need to be the pool or maybe it needs to be a variable passed into the _init() function
+
+func _init():
+	pass
+
+func cast(player_position, mouse_position):
+	pass
+	
+func impact_enemy(power, body):
+	pass
+	
+func set_attributes(new_attributes):
+	pass
+	
+func get_attributes():
+	pass
+	
