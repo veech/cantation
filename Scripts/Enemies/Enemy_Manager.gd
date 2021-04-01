08d8d085036b5d52f272a40f5d27219d37f851a0
Reed Tucker <reeedt@gmail.com>
2021-04-01 18:36:37 -0400

    cleaned up code

--- Scripts/Enemies/Enemy_Manager.gd
+++ Scripts/Enemies/Enemy_Manager.gd
@@ -1,10 +1,6 @@
-#So this enemy manager is basically functional rn.
-#however I need it to calculate a radius to spawn enemies
-#Enemies will be spawned along this radius
-#depending on the value of the radius, I need a max number of enemies calculated
-#that will take into consideration the size of the enemies and spacing so they dont overlap
-
-
+#If unexpected behavior is occuring with enemy movement and placement
+#check the hard coded value for enemy height and width
+#this should change eventually but idk how to do this
 extends Node2D
 
 
@@ -13,19 +9,17 @@ var timer
 var update_interval = .3
 
 var player_detected = false
-#Temp var for naming the spawned enemies
 var spawn_point_name = "Enemy_spawn"
 
-
-export(int, 10, 100) var spawn_radius = 10
+export(int, 9, 100) var spawn_radius = 40
 export var enemies_spawned = 12
 
 var min_spawn_angle: float
-var tallest_enemy_height: float = 32
+var enemy_height: float = 32
 var enemy_width: float = 16
 
 onready var enemy_container = $Enemy_Container
-onready var Enemy = preload("res://Scenes/Enemies/Enemy.tscn")
+export(PackedScene) onready var Enemy = preload("res://Scenes/Enemies/Enemy.tscn")
 
 func _ready():
 	spawn_enemies()
@@ -33,7 +27,6 @@ func _ready():
 	timer = Timer.new()
 	add_child(timer)
 	timer.set_one_shot(false)
-#	timer.set_timer_process_mode(TIMER_PROCESS_FIXED)
 	timer.set_wait_time(update_interval)
 	timer.connect("timeout", self, "update_goal")
 	timer.start()
@@ -46,12 +39,22 @@ func spawn_enemies():
 		var new_spawn = Enemy.instance()
 		new_spawn.set_name(spawn_point_name + "_" + str(i))
 		$Enemy_Container.add_child(new_spawn)
-		#new_spawn.position = calculate_spawn_position(i)
-		if new_spawn.extents.y*2 > tallest_enemy_height:
-			tallest_enemy_height = new_spawn.extents.y*2
 		enemies.push_back(new_spawn)
 	place_enemies()
-
+	
+func calculate_max_enemies():
+	var max_enemies: int
+	if spawn_radius < enemy_width:
+		max_enemies = 1
+	elif spawn_radius < enemy_height:
+		max_enemies = 2
+	else:
+		min_spawn_angle = asin(enemy_height/spawn_radius)
+		var full_circle = PI * 2
+		var num_of_angles = full_circle/min_spawn_angle
+		max_enemies = int(num_of_angles)	
+	return max_enemies
+	
 func place_enemies():
 	var placement_angle_interval = (2*PI)/len(enemies)
 	var placement_angle = placement_angle_interval
@@ -59,32 +62,9 @@ func place_enemies():
 		enemy.position = Vector2((spawn_radius * cos(placement_angle)), (spawn_radius * sin(placement_angle)))
 		placement_angle += placement_angle_interval
 
-func calculate_circle():
-	min_spawn_angle = asin(tallest_enemy_height/spawn_radius)
-
-func calculate_max_enemies():
-	var max_enemies: int
-	if spawn_radius < enemy_width:
-		max_enemies = 1
-	elif spawn_radius < tallest_enemy_height:
-		max_enemies = 2
-	else:
-		min_spawn_angle = asin(tallest_enemy_height/spawn_radius)
-		var full_circle = PI * 2
-		var num_of_angles = full_circle/min_spawn_angle
-		max_enemies = int(num_of_angles)	
-	return max_enemies
-
 func update_goal():
 	if player_detected:
-		print("updating goals")
 		for i in len(enemies):
 			if enemies[i] != null:
 				enemies[i].calculate_goal()
 
-func calculate_spawn_position(index):
-	var starting_position = spawn_radius
-	var spawn_interval = spawn_radius / (enemies_spawned+1)
-	return Vector2((spawn_interval - (spawn_interval * index)), 0)
-	
-	
--- Scripts/Inventory/Spells_Set.gd
+++ Scripts/Inventory/Spells_Set.gd
@@ -11,9 +11,3 @@ func _ready():
 		slots.push_back(i)
 		i.slotIndex = counter
 		counter += 1
-		print(i, "Slot index = ", i.slotIndex)
-
-#	slots.push_front(get_node("Spell_Slots/Spell_D"))
-#	slots.push_front(get_node("Spell_Slots/Spell_C"))
-#	slots.push_front(get_node("Spell_Slots/Spell_B"))
-#	slots.push_front(get_node("Spell_Slots/Spell_A"))
